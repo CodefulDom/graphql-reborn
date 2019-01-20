d8d03faaf1a79bec29ff3ca91ae615983268c19f
codefuldom <dominiquehallan@gmail.com>
2019-01-20 17:56:08 -0600

    :star2: module completed!

--- graphql-basics/src/myModule.js
+++
@@ -1,9 +0,0 @@
-const message = 'Some message from myModule.js'
-const name = 'Dom Hallan'
-const city = 'STL'
-
-const getGreeting = name => {
-  return(`Hello, ${name}!`)
-}
-
-export{ message, name, getGreeting, city as default }
